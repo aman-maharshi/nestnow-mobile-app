ae3f2afd63ab0a34cf549cccd4d8f6874606b1a5
aman-maharshi <amanm4harshi@gmail.com>
2025-08-21 20:03:16 +0530

    assets and constants updates

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,7 +11,10 @@ export default function Index() {
         alignItems: "center"
       }}
     >
-      <Text className="text-2xl font-bold text-green-700">Welcome to NestNow</Text>
+      <Text className="text-2xl font-rubik-bold text-green-700">Welcome to NestNow</Text>
+      <Text className="font-rubik-regular text-black-100 text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </Text>
       <Button title="Sign In" onPress={() => router.push("/sign-in")} />
       <Button title="Explore" onPress={() => router.push("/explore")} />
       <Button title="Profile" onPress={() => router.push("/profile")} />
